b1a6f3856628805e0c08035bbebcc21f726b07b3
James Lew <jamseblew@gmail.com>
2021-02-07 16:16:59 +1100

    reformatting folder structure

---
+++ scripts/settings.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+# using .env file to grab private token
+env_path= Path('.') / '..' / '.env'
+load_dotenv(dotenv_path = env_path)
+
+TOKEN       = os.getenv("TOKEN")
+PREFIX      = "yeetus"
